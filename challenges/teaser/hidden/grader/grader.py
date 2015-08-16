e5872f573a41025a188d92b22bbd09380df7d358
Koh Jun Xiang <junxiang1991@gmail.com>
2015-08-16 23:26:14 +0800

    Added teaser\hidden challenge

---
+++ challenges/teaser/hidden/grader/grader.py
@@ -0,0 +1,5 @@
+def grade(arg, key):
+    if "flag{htm!_qrc0d3}".lower() == key.lower():
+        return True, "=)"
+    else:
+        return False, "..."
---
+++ challenges/teaser/hidden/static/hidden.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <title>CTF101 - Hidden</title>
+	    <style> .b {background: #000000;} .w{background: #000000;}</style>
+    </head>
+    <body style="margin:auto;padding-top:50px;background:black;color:#0F0;">
+        <div align="center">
+            <pre class="logo" style="font-size:6px;">
+          _____                ____                    _____                             
+         /\    \              /\    \                  /\    \                           
+        /██\    \            /██\    \                /██\    \                          
+       /████\    \           \███\    \              /████\    \                         
+      /██████\    \           \███\    \            /██████\    \                        
+     /███/\███\    \           \███\    \          /███/\███\    \                       
+    /███/  \███\    \           \███\    \        /███/__\███\    \                      
+   /███/    \███\    \          /████\    \      /████\   \███\    \                     
+  /███/    / \███\    \        /██████\    \    /██████\   \███\    \                    
+ /███/    /   \███\    \      /███/\███\    \  /███/\███\   \███\    \                   
+/███/____/     \███\____\    /███/  \███\____\/███/  \███\   \███\____\                  
+\███\    \      \██/    /   /███/    \██/    /\██/    \███\   \██/    /                  
+ \███\    \      \/____/   /███/    / \/____/  \/____/ \███\   \/____/                   
+  \███\    \              /███/    /                    \███\    \                       
+   \███\    \            /███/    /                      \███\____\                      
+    \███\    \           \██/    /                        \██/    /  ██╗ ██████╗  ██╗    
+     \███\    \           \/____/                          \/____/  ███║██╔═████╗███║    
+      \███\    \                                                    ╚██║██║██╔██║╚██║    
+       \███\____\                                                    ██║████╔╝██║ ██║    
+        \██/    /                                                    ██║╚██████╔╝ ██║    
+         \/____/                                                     ╚═╝ ╚═════╝  ╚═╝    
+</pre>
+			<p>Can you see the hidden flag?</p>
+			<table style="width:75px; height:75px;" cellspacing="0" cellpadding="0">
+             <tbody>
+				<tr><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td></tr>                
+				<tr><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td></tr>                                                                                                                                                                                                                         
+                <tr><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td></tr>
+				<tr><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="w"></td></tr>
+                <tr><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="b"></td><td class="w"></td><td class="b"></td><td class="w"></td></tr>
+			 </tbody>
+			</table>
+			<p><a href="hiddenflag.php"><font color="black">Click me to get flag!</font></a></p>
+        </div>
+    </body>
+
+</html>
