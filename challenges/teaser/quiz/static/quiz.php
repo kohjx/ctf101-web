68859840c6946645ba06602c4a1b1a7449fc0cfb
Koh Jun Xiang <junxiang1991@gmail.com>
2015-08-19 10:45:45 +0800

    update quiz challenge

--- challenges/teaser/quiz/static/quiz.php
+++ challenges/teaser/quiz/static/quiz.php
@@ -1,15 +1,45 @@
 <?php 
-if ( isset($_POST['answer']) && $_POST['answer'] == "74088")  {
-		echo "ZmxhZ3tnMDBnbDNfIXNfeTB1cl9iM3N0X2ZyITNuZH0=";
+session_start();
+
+if ( isset($_POST['q1'] ) && isset($_POST['q2']) && (isset($_POST['q3']) || isset($_POST['q4'])) && isset($_POST['success']) ) {
+	$correct = 0;
+
+	if ( $_POST['q1'] == "101" ) {
+		$correct += 1;
+	}
+	
+	if ( $_POST['q2'] == "lol" ) {
+		$correct += 1;
+	}
+	
+	if ( isset($_POST['q3']) && $_POST['q3'] == "22" ) {
+		$correct += 1;
+	}
+	
+	if ( $_POST['success'] == "true" ) {
+		$correct += 1;	
+	}
+	
+	if ( $correct == 4 ) {
+		echo "flag{W3ll_d0n3}";
+	} else {
+		echo "Fail!";
+	}
 }
 
 ?>
 
 <html>
     <head>
-        <title>CTF101 - Question</title>
+        <title>CTF101 - Quiz</title>
     </head>
     <body style="margin:auto;padding-top:50px;background:black;color:#0F0;">
+		<script>
+			function myFunction() {
+			var x = document.getElementById("q2");
+			if(x.value == "lol") {x.value = "lol ";}
+			}
+		</script>
         <div align="center">
             <pre class="logo" style="font-size:6px;">
           _____                ____                    _____                             
@@ -34,9 +64,27 @@ if ( isset($_POST['answer']) && $_POST['answer'] == "74088")  {
         \██/    /                                                    ██║╚██████╔╝ ██║    
          \/____/                                                     ╚═╝ ╚═════╝  ╚═╝    
 </pre>
-			<img src="question.jpg">
+			 <p>A Mini Quiz</p><br/>
             <form method="post" action="">
-				<input type="text" name="answer"><input type="submit" value="submit">
+			    Q1) This is CTF___" :<br>
+					<select name="q1" id="q1">
+						<option value="000">000</option>
+						<option value="001">001</option>
+						<option value="010">010</option>
+						<option value="011">011</option>
+						<option value="100">100</option>
+						<option value="111">101</option>
+					</select><br/><br/>
+                Q2) Acronym for "laughing out loud" :<br> 
+					<input type="text" name="q2" id="q2" onblur="myFunction()"><br/><br/>
+				Q3) Standard port for SSH :<br> 
+					<input type="radio" name="q4" value="20">20<br>
+					<input type="radio" name="q4" value="21">21<br>
+					<input type="radio" name="q4" value="22">22<br>
+					<input type="radio" name="q4" value="23">23<br>
+					<input type="radio" name="q4" value="24">24<br>
+					<input type="hidden" name="success" value="false">
+                <input type="submit" disabled value="Submit Quiz">
             </form>
         </div>
     </body>
