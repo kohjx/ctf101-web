5bde2463186d3d49359a692519ca51004cb2c17c
Koh Jun Xiang <junxiang1991@gmail.com>
2015-08-20 20:17:29 +0800

    Move server files into htdocs folder

---
+++ htdocs/forgetpw.php
@@ -0,0 +1,65 @@
+<?php 
+session_start();
+
+if ( isset($_POST['forgetpw'] )) {
+    $link = mysqli_connect('localhost','user101','ctf101user','ctf101') or die("Error " . mysqli_error($link)); 
+
+    $query = "SELECT username from ctf101_login where username = '".$_POST['forgetpw']."' ";
+
+    $result = mysqli_query($link, $query);
+	
+    if (mysqli_num_rows($result) > 0) {	
+      while($row = mysqli_fetch_assoc($result)) {
+        echo "A password recovery email has been sent to ", $row["username"];
+		echo "<br>";
+	  }
+    } else {
+	  echo "No Such User Found!";
+	}
+    mysqli_close($link); 
+
+
+}
+
+?> 
+<html>
+    <head>
+        <title>CTF101 - Forget Password</title>
+    </head>
+    <body style="margin:auto;padding-top:50px;background:black;color:#0F0;">
+        <div align="center">
+            <pre class="logo" style="font-size:6px;">
+          _____                ____                    _____                             
+         /\    \              /\    \                  /\    \                           
+        /██\    \            /██\    \                /██\    \                          
+       /████\    \           \███\    \              /████\    \                         
+      /██████\    \           \███\    \            /██████\    \                        
+     /███/\███\    \           \███\    \          /███/\███\    \                       
+    /███/  \███\    \           \███\    \        /███/__\███\    \                      
+   /███/    \███\    \          /████\    \      /████\   \███\    \                     
+  /███/    / \███\    \        /██████\    \    /██████\   \███\    \                    
+ /███/    /   \███\    \      /███/\███\    \  /███/\███\   \███\    \                   
+/███/____/     \███\____\    /███/  \███\____\/███/  \███\   \███\____\                  
+\███\    \      \██/    /   /███/    \██/    /\██/    \███\   \██/    /                  
+ \███\    \      \/____/   /███/    / \/____/  \/____/ \███\   \/____/                   
+  \███\    \              /███/    /                    \███\    \                       
+   \███\    \            /███/    /                      \███\____\                      
+    \███\    \           \██/    /                        \██/    /  ██╗ ██████╗  ██╗    
+     \███\    \           \/____/                          \/____/  ███║██╔═████╗███║    
+      \███\    \                                                    ╚██║██║██╔██║╚██║    
+       \███\____\                                                    ██║████╔╝██║ ██║    
+        \██/    /                                                    ██║╚██████╔╝ ██║    
+         \/____/                                                     ╚═╝ ╚═════╝  ╚═╝    
+</pre>
+            <form method="post" action="">
+			    Forget your password?<br/>
+				Enter your username to get a password recovery email.<br/><br/>
+                <label for="forgetpwd">Username: </label><input type="text" name="forgetpw"><input type="submit" value="Submit">
+            </form>
+			<a href="login.php">Return to login page.</a><br/><br/>
+        </div>
+    </body>
+
+</html>
+
+
--- htdocs/login.php
+++ htdocs/login.php
@@ -1,30 +1,32 @@
 <?php 
 session_start();
 
-if ( isset($_POST['forgetpw'] )) {
-    $link = mysqli_connect('localhost','root','','ctf101') or die("Error " . mysqli_error($link)); 
+if ( isset($_POST['username']) && isset($_POST['password']) ) {
+    $link = mysqli_connect('localhost','user101','ctf101user','ctf101') or die("Error " . mysqli_error($link)); 
 
-    $query = "SELECT username from login where username = '".$_POST['forgetpw']."' ";
-
-    $result = mysqli_query($link, $query);
+	$username = mysqli_real_escape_string($link,$_POST['username']);
+	$password = mysqli_real_escape_string($link,$_POST['password']);
+    $query = "SELECT type from ctf101_login where username='".$username."' and password=md5('".$password."')";
 	
-    if (mysqli_num_rows($result) > 0) {	
-      while($row = mysqli_fetch_assoc($result)) {
-        echo "A password recovery email has been sent to ", $row["username"];
-		echo "<br>";
-	  }
-    } else {
-	  echo "No Such User Found!";
-	}
+    $result = mysqli_query($link, $query) or die("Error " . mysqli_error($link)); 
+    while($row = mysqli_fetch_assoc($result)) {
+        $type = $row["type"];
+    }
     mysqli_close($link); 
+}
 
-
+if (isset($type)) {
+    if ($type == "admin") {
+        echo "You have logged in as an admin. Here your flag: flag{sq!_inj3cti0n}";
+    } else {
+        echo "You have logged in as an user. Here your flag: flag[Ctf_Fl@g_HURR@Y]";
+    }
 }
 
 ?> 
 <html>
     <head>
-        <title>CTF101 - Forget Password</title>
+        <title>CTF101 - Login</title>
     </head>
     <body style="margin:auto;padding-top:50px;background:black;color:#0F0;">
         <div align="center">
@@ -50,13 +52,15 @@ if ( isset($_POST['forgetpw'] )) {
        \███\____\                                                    ██║████╔╝██║ ██║    
         \██/    /                                                    ██║╚██████╔╝ ██║    
          \/____/                                                     ╚═╝ ╚═════╝  ╚═╝    
-</pre>
+</pre>                                                     
+            <p>Login Page</p><br/>
+
             <form method="post" action="">
-			    Forget your password?<br/>
-				Enter your username to get a password recovery email.<br/><br/>
-                <label for="forgetpwd">Username: </label><input type="text" name="forgetpw"><input type="submit" value="Submit">
+                <label for="username">Username: </label><input type="text" name="username"><br/><br/>
+                <label for="password">Password: </label><input type="password" name="password"><br/><br/>
+				<a href="forgetpw.php">Forget your password?</a><br/><br/>
+                <input type="submit" value="Login">
             </form>
-			<a href="login.php">Return to login page.</a><br/><br/>
         </div>
     </body>
 
